5d8289c9198645ceede987f467e9112e05bbbca8
Damon R <dmarcr1997@gmail.com>
2020-01-23 22:04:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ require 'pry'
 # Write your code here.
 def line(line_arr)
   if line_arr.count() == 0
-    return "The line is currently empty"
+    puts "The line is currently empty"
   else
     return line_arr.count() + 1
   end
