0a9fcd7248b73331a2ae9e5fa48d79711ddb4f4d
Damon R <dmarcr1997@gmail.com>
2020-01-23 22:10:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,27 @@
 require 'pry'
 # Write your code here.
 def line(line_arr)
+  line_string= "The line is currently: "
   if line_arr.count() == 0
     puts "The line is currently empty"
     return false
   else
-    return line_arr.count() + 1
+    line_arr.each_with_index do |item, index|
+      line_string << "#{index+1}. #{item} "
+    end
+    puts line_string
   end
+  return line_arr(count)+1
 end
 
 def take_a_number(line_arr, name)
   line_arr << name
   place = line(line_arr)
-  puts "#{name} your place in line is #{place}"
+  puts "Welcome, #{name}. You are number #{place} in line."
 end
-binding.pry
+
 
 def now_serving(line_arr)
   next_name = line_arr.shift
   if line(line_arr) == false
-    
+    puts"There is nobody waiting to be served!"
